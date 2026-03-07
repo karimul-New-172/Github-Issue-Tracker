475128e852eebc361ea4a1d084eba979892080ab
karimul-New-172 <karimul172@gmail.com>
2026-03-07 22:30:40 +0600

    log functionality added

---
+++ login.js
@@ -0,0 +1,16 @@
+document.getElementById('btn-sign-in').addEventListener('click', function(){
+    const userNameField = document.getElementById('username');
+    const userInfo = userNameField.value;
+    const passwordField = document.getElementById('password');
+    const passInfo = passwordField.value;
+    console.log(userInfo, passInfo);
+
+    if(userInfo === "admin" && passInfo === "admin123"){
+        window.location.href= './index.html'
+        
+        console.log('hello');
+    }else{
+        alert('Plese enter default user and password');
+        return;
+    }
+})
